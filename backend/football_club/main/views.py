ab6b3ec32d6b8ae93220f5ba5e11587334b462f9
bereket kume <bereketkume@gmail.com>
2024-05-25 12:44:28 -0700

    done

--- backend/football_club/main/views.py
+++ backend/football_club/main/views.py
@@ -3,18 +3,18 @@ from rest_framework import generics
 from .models import Player, News
 from .serializers import PlayerSerializer, NewsSerializer
 
-class PlayerListCreate(generics.ListCreateAPIView):
+class PlayerListCreate(generics.ListAPIView):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
-class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+class PlayerDetail(generics.RetrieveAPIView):
     queryset =Player.objects.all()
     serializer_class = PlayerSerializer
 
-class NewsListCreate(generics.ListCreateAPIView):
+class NewsListCreate(generics.ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
-class NewsDetail(generics.RetrieveUpdateDestroyAPIView):
+class NewsDetail(generics.RetrieveAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
